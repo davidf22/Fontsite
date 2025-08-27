8a4533b117617ac8168b11731528e75a7edf5052
emzzy <ezehemmanuel265@gmail.com>
2025-08-27 23:46:41 +0100

    Added Gender and OTP field for default User model

--- userauth/models.py
+++ userauth/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+GENDER = (
+    ("Female", "Female"),
+    ("Male", "Male"),
+    ("Other", "Other"),
+)
+
 class UserAccount(AbstractUser):
     email = models.EmailField(unique=True, max_length=500)
     username = models.CharField(max_length=100, null=True, blank=True)
     password = models.CharField(max_length=100, null=True, blank=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
+    gender = models.CharField(max_length=20, choices=GENDER, default='Other')
+    otp = models.CharField(max_length=100, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
