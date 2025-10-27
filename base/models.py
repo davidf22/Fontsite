249bd9dc9c6c92370223e74fdd7b0c928325a385
emzzy <ezehemmanuel265@gmail.com>
2025-10-27 23:32:31 +0000

    Added Venue model

--- base/models.py
+++ base/models.py
@@ -1,8 +1,51 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+from django.utils.html import mark_safe
 from django.core.validators import MaxValueValidator
+from shortuuid.django_fields import ShortUUIDField
+import shortuuid
 
 User = get_user_model()
+
+HOTEL_STATUS = (
+    ("Draft", "Draft"),
+    ("Disabled", "Disabled"),
+    ("Rejected", "Rejected"),
+    ("In Review", "Draft"),
+    ("Live", "Live"),
+)
+
+class Venue(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    image = models.FileField(upload_to="hotel_glallery")
+    address = models.CharField(max_length=200)
+    mobile = models.CharField(max_length=200)
+    email = models.EmailField(max_length=36)
+    status = models.CharField(max_length=20, choices=HOTEL_STATUS)
+
+    tags = models.CharField(max_length=200, help_text="Seperate tags with comma")
+    views = models.IntegerField(default=0)
+    featured = models.BooleanField(default=False)
+    hid = ShortUUIDField(unique=True, length=10, max_length=20, alphabet="abcdefghijklmnopqrstuvwxyz")
+    slug = models.SlugField(unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            uuid_key = shortuuid.uuid()
+            uniqueid = uuid_key[:4]
+            self.slug = slugify(self.name) + '-' + str(uniqueid.lower())
+
+        super(Venue, self).save(*args, **kwargs)
+    
+    def thumbnail(self):
+        return mark_safe("<img_src='%s' width='50' height='50' style='object-fit: cover; border-radius: 6px;' />" % (self.image.url))
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='user_booking')
